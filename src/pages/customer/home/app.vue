73d251def347ecbd1fab0a19e8ba8b182462f8ce
zhongqi7777 <yanlibo@300.cn>
2020-12-17 10:56:17 +0800

    add src

---
+++ src/pages/customer/home/app.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="app">
+    <div class="dd">
+        Home
+    </div>
+    <el-button @click.native="startHacking">Let's do it</el-button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+
+  methods: {
+    startHacking() {
+      this.$notify({
+        title: 'It Works',
+        message:
+          "We have laid the groundwork for you. Now it's your time to build something epic!",
+        duration: 6000
+      })
+    }
+  }
+}
+</script>
+
+<style lang="postcss">
+body {
+  background-color: red;
+}
+
+:root h1 {
+  --color: red;
+  display: flex;
+  color: var(--color);
+}
+</style>
